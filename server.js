dd308045932d69dfcea99330eb20f25b924aa960
Nguyen Dat <tiendat13895@gmail.com>
2018-06-30 13:11:14 +0700

    Add Server

--- server.js
+++ server.js
@@ -9,3 +9,22 @@ server.use(router)
 server.listen(3000, () => {
   console.log('JSON Server is running')
 })
+
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/angular'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/angular/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080, ()=>{
+  console.log('server is runing on', 8080)
+});
