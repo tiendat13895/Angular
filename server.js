f7f8d685318d5bc2f7cb7a58fd70572595771faa
Nguyen Dat <tiendat13895@gmail.com>
2018-06-30 13:27:50 +0700

    Add Server

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ res.sendFile(path.join(__dirname+'/dist/angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080, ()=>{
-  console.log('server is runing on', 8080)
+app.listen(process.env.PORT || 80, ()=>{
+  console.log('server is runing on', 80)
 });
