a155c8c246f17090fbece55b76dfe707d3b9fb3b
dat13895 <tiendat13895@gmail.com>
2018-07-04 19:46:53 +0700

    Update Server

--- server.js
+++ server.js
@@ -1,30 +1,16 @@
-// // server.js
-// const jsonServer = require('json-server')
-// const server = jsonServer.create()
-// const router = jsonServer.router('db.json')
-// const middlewares = jsonServer.defaults()
-
-// server.use(middlewares)
-// server.use(router)
-// server.listen(3000, () => {
-//   console.log('JSON Server is running')
-// })
-
-//Install express server
 const express = require('express');
 const path = require('path');
 var jsonServer = require('json-server');
 
 const app = express();
-app.use('api/', jsonServer.router('db.json'));
+app.use('/api', jsonServer.router('db.json'));
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
-
 // Start the app by listening on the default Heroku port
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, ()=>{
-  console.log(`server is runing on, ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server started in port: ${PORT}`);
 });
